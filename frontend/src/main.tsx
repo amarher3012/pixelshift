063289c5c21cd33a52a24f64ea76a0edd497715c
Alejandro Martin Herrera <amarher3012@fp.iesromerovargas.com>
2025-04-09 16:49:20 +0200

    Add react-router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.tsx';
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router'
+
+import './index.css'
+import Home from './pages/Home'
 
 createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <App />
-    </StrictMode>
-);
+    <BrowserRouter>
+        <Routes>
+            <Route index element={<Home />} />
+        </Routes>
+    </BrowserRouter>
+)
--- frontend/src/routes/home.tsx
+++
@@ -1,3 +0,0 @@
-import { type RouteConfig, route } from '@react-router/dev/routes'
-
-export default [route('/', '../pages/Home')] satisfies RouteConfig
